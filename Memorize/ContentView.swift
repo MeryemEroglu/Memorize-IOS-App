5cd036d9a218656bd2af3c13c5be7a7b80182294
MeryemEroglu <200101022@ogrenci.yalova.edu.tr>
2024-12-19 21:23:33 +0300

    Initial Commit

---
+++ Memorize/ContentView.swift
@@ -0,0 +1,50 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Meryem Eroğlu on 18.12.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        HStack{
+            CardView(isFaceUp: true)
+            CardView()
+            CardView(isFaceUp: true)
+            CardView()
+        }
+        .foregroundColor(.orange)
+        .padding()
+    }
+        
+}
+struct CardView: View {
+    var isFaceUp: Bool = false
+    
+    var body: some View {
+        ZStack(content: {
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: 12)
+                    .foregroundColor(.white)
+                RoundedRectangle(cornerRadius: 12)
+                    .strokeBorder(lineWidth: 2)
+                Text("🍏").font(.largeTitle)
+            } else {
+                RoundedRectangle(cornerRadius: 12)
+                }
+        })
+        
+    }
+}
+
+
+
+
+
+
+
+#Preview {
+    ContentView()
+}

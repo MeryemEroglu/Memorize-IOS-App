c2de586894f0fce0f1374a1af0b9cf050a6430f5
MeryemEroglu <200101022@ogrenci.yalova.edu.tr>
2025-03-17 17:06:04 +0300

    Toggleable isFaceUp

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -10,32 +10,32 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         HStack{
-            CardView(isFaceUp: true)
+            CardView(isFaceUp: false)
             CardView()
             CardView(isFaceUp: true)
-            CardView()
+            CardView(isFaceUp: true)
         }
         .foregroundColor(.orange)
         .padding()
     }
-        
 }
 struct CardView: View {
-    var isFaceUp: Bool = false
+    @State var isFaceUp = false
     
     var body: some View {
-        ZStack(content: {
+        ZStack {
+            let base: RoundedRectangle = RoundedRectangle(cornerRadius: 12)
             if isFaceUp {
-                RoundedRectangle(cornerRadius: 12)
-                    .foregroundColor(.white)
-                RoundedRectangle(cornerRadius: 12)
-                    .strokeBorder(lineWidth: 2)
+                base.fill(.white)
+                base.strokeBorder(lineWidth: 2)
                 Text("🍏").font(.largeTitle)
             } else {
-                RoundedRectangle(cornerRadius: 12)
+                base.fill()
                 }
-        })
-        
+        }
+        .onTapGesture{
+            isFaceUp.toggle()
+        }
     }
 }
 
